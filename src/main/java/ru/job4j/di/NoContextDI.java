c8f572f816adf588edc6833e0d2042a06ae8bbd9
itlazykin <dzaka89@gmail.com>
2025-01-24 15:46:07 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/NoContextDI.java
+++ src/main/java/ru/job4j/di/NoContextDI.java
@@ -4,7 +4,7 @@ public class NoContextDI {
     public static void main(String[] args) {
         Store store = new Store();
         ConsoleInput consoleInput = new ConsoleInput();
-        StartUI iu = new StartUI();
+        StartUI iu = new StartUI(store, consoleInput);
         iu.add("Den");
         iu.add("Lazykin");
         iu.print();
--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -7,10 +7,17 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
+        Store store = context.getBean(Store.class);
+        store.add("Den Lazykin");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
         StartUI ui = context.getBean(StartUI.class);
         ui.add("Den Lazykin");
         ui.add("Nasty Lazykina");
-        ui.print();
         ui.question();
+        StartUI anotherUi = context.getBean(StartUI.class);
+        anotherUi.add("anotherUi");
+        anotherUi.print();
+        anotherUi.question();
     }
 }
